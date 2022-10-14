50fd6b02b721151aa4689173908250299da1d7fd
Simon Brummer <simon.brummer@gmail.com>
2022-10-14 16:11:32 -0400

    second commit

--- src/main/java/com/brummer/brummermongoreact/brummermongoreact/HomeRestController.java
+++ src/main/java/com/brummer/brummermongoreact/brummermongoreact/HomeRestController.java
@@ -1,9 +1,6 @@
 package com.brummer.brummermongoreact.brummermongoreact;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.brummer.brummermongoreact.brummermongoreact.test.Test;
